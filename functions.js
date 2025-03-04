112df0edc7b72291ad98a7ceecff1df28e732ada
Kareena2070 <kareenayadav1088@gmail.com>
2025-03-04 18:10:18 +0530

    solve day in an year and sum of two number DSA question

---
+++ functions.js
@@ -0,0 +1,23 @@
+// Declare a function named daysInAnYear. Inside this function, write a return statement that returns the number 365.
+
+// Call the daysInAnYear function after declaring it, and store the returned value in a variable called days and console it.
+
+function daysInAnYear() {
+    return 365;
+   }
+    let days = daysInAnYear();
+   
+   console.log(days)
+
+
+//    Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+
+// Call the function with two numbers as arguments.
+
+function functionWithArgs(a, b){
+    return a+b;
+  }
+   
+  console.log(functionWithArgs(1,2));
+  console.log(functionWithArgs(7,9));
+  
