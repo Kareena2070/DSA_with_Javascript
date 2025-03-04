cec11b884a0c4383b0843b89e0ab94258cf49b02
Kareena2070 <kareenayadav1088@gmail.com>
2025-03-04 22:12:16 +0530

    multiplies it by 5 and order of logic DSA question

--- functions.js
+++ functions.js
@@ -21,3 +21,46 @@ function functionWithArgs(a, b){
   console.log(functionWithArgs(1,2));
   console.log(functionWithArgs(7,9));
   
+
+  // Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+  function timesFive(num) {
+    return num * 5;
+  }
+  
+  const a = timesFive(5);
+  const b = timesFive(2);
+  const c = timesFive(0);
+  console.log(a);
+  console.log(b);
+  console.log(c);
+
+  
+//   Change the order of logic in the function so that it will return the correct statements in all cases.
+function orderMyLogic(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+console.log(orderMyLogic(7));
+
+
+
+
+
