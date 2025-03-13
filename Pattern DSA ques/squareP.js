e1721ce79b04f793bf477336af91367f01c5c169
Kareena2070 <kareenayadav1088@gmail.com>
2025-03-13 15:35:17 +0530

    add square pattern question solution

--- Pattern DSA ques/squareP.js
+++ Pattern DSA ques/squareP.js
@@ -34,3 +34,65 @@ star(3);
 // output    ****
         //   ****
         //   ****
+
+
+
+ function apl(n){
+    for(let i = 0; i < n; i++){
+         let row = ""
+        let char = "A"
+        for(let j = 0; j < n; j++){
+            row += char;
+            char = String.fromCharCode(char.charCodeAt(0) + 1); // Move to next character
+        }
+        console.log(row);
+    }
+ }       
+
+apl(3);
+
+// output  ABC
+        // ABC
+        // ABC
+
+
+ function continueNum(n){
+    let num = 1;
+    for(let i = 0; i < n; i++){
+        let row = ""
+        for(let j=0; j < n; j++){
+            row += num;
+            num++;
+        }
+       
+        console.log(row);
+    }
+    console.log("After pattern num will be :", num);
+ }       
+
+ continueNum(3);
+
+// output 123
+//        456
+//        789
+//        After pattern num will be : 10
+
+
+ function continueApl(n){
+    let ch = "A";
+    for(let i=0; i <n; i++){
+        let row = "";
+        for(let j = 0; j < n; j++){
+            row += ch;
+            ch = String.fromCharCode(ch.charCodeAt(0) + 1);
+            
+        }
+        console.log(row);
+    }
+ }
+
+ continueApl(3);
+ 
+//  output ABC
+//         DEF
+//         GHI
