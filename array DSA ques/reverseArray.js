066e7e3f58fedc73641930703876e751c4eee2d4
Kareena2070 <kareenayadav1088@gmail.com>
2025-08-04 18:33:02 +0530

    solve reverse array with no extra space

--- array DSA ques/reverseArray.js
+++ array DSA ques/reverseArray.js
@@ -2,20 +2,37 @@
 
 // reverseAndPrintArray(L); // Output: 90 71 33 46 25 32 45 
 
-
-
-const L = [23, 45, 71, 90];
+const arr = [23, 45, 71, 90];
 
 function reverseAndPrintArray(arr) {
     const reversedArray = []
-   
- for (let i = arr.length - 1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         reversedArray.push(arr[i]);
     }
-   
    return(reversedArray);
 }
 
-reverseAndPrintArray(L);
+console.log(reverseAndPrintArray(arr));
+// time complexity = O(n) one loop is running
+// space complexity = O(n) whole array store again in new variables
 
-console.log(reverseAndPrintArray(L));
+
+
+// Reverse an array
+let arr1 = [2, 3, 5, 6, 8, 9]
+
+function reverseArray(arr){
+    left = 0;
+    right = arr.length - 1;
+
+    while(left<right){
+        [arr[left], arr[right]] = [arr[right], arr[left]];    // in swap js ([something] = [something])
+        left++;
+        right--;
+    }
+    return arr
+}
+
+console.log(reverseArray(arr1))
+// time complexity = O(n)
+// Space complexity = O(1) no, new any variables
